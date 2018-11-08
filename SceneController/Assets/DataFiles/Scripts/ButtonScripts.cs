a642682888aba4b70fc08c0b1f30c814165a1666
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2018-11-08 17:16:05 +0530

    Project 4

---
+++ SceneController/Assets/DataFiles/Scripts/ButtonScripts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScripts : MonoBehaviour {
+    public void greetingcard()
+    {
+        SceneManager.LoadScene("GreetingCardScene");
+    }
+    public void rotatecube()
+    {
+        SceneManager.LoadScene("RotateCube");
+    }
+    public void exitgame()
+    {
+        Application.Quit();
+        Debug.Log("Exit Button Pressed");
+    }
+}
---
+++ SceneController/Assets/DataFiles/Scripts/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () { 
+        Debug.Log("Hello");
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(40, 30, 30) * Time.deltaTime);
+
+    }
+}
